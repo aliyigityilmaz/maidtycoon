19a7621bd11374127a1a7ae6896786771dd0b982
LariXv1 <berkaysagdic@hotmail.com>
2024-05-27 18:00:43 +0300

    sdfdsf

---
+++ Assets/Scripts/Cutscene/NormalSahneDiyalog.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NormalSahneDiyalog : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        index = 0;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        StartCoroutine(TypeLine());
+        GameManager.instance.isGameStarted = false;
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            GameManager.instance.StartGame();
+            Destroy(this.gameObject);
+        }
+    }
+}
